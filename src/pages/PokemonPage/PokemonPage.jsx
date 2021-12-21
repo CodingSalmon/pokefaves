6da3e2a746d423904d8daaaa07718bc212431466
CodingSalmon <sgemberling@gmail.com>
2021-12-21 11:13:44 -0600

    update commenting code

--- src/pages/PokemonPage/PokemonPage.jsx
+++ src/pages/PokemonPage/PokemonPage.jsx
@@ -11,59 +11,47 @@ import {
 } from "../../services/comment-api";
 
 const PokemonPage = ({ user, handleFavorite }) => {
-  const [state, setState] = useState({
-    pokemon: {},
-    comments: [],
-    formData: {
-      msg: "",
-      pokemonName: "",
-    },
-  });
-
   const { pokemonName } = useParams();
+
+  const [pokemon, setPokemon] = useState({});
+  const [comments, setComments] = useState([]);
+  const [formData, setFormData] = useState({
+    msg: "",
+    pokemonName: pokemonName,
+  });
 
   useEffect(() => {
     (async () => {
-      const pokemon = await getPokemonDetails(pokemonName);
+      const pokemonDetails = await getPokemonDetails(pokemonName);
       const comments = await getComments(pokemonName);
-      setState({ pokemon, comments, formData: { pokemonName: pokemon.name } });
+      setPokemon(pokemonDetails);
+      setComments(comments);
     })();
   }, [pokemonName]);
 
   const handleChange = (e) => {
-    const formData = { ...state.formData, [e.target.name]: e.target.value };
-    setState({ formData });
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const addComment = async (e) => {
     e.preventDefault();
-    const comment = await createComment(state.formData);
-    setState({
-      ...state,
-      comments: [...state.comments, comment],
-      formData: { msg: "", pokemonName: state.pokemon.name },
-    });
+    const newComment = await createComment(formData);
+    setComments([...comments, newComment]);
+    setFormData({ ...formData, msg: "" });
   };
 
   const deleteComment = async (id) => {
     await removeComment(id);
-    setState({
-      ...state,
-      comments: state.comments.filter((comment) => comment._id !== id),
-    });
+    setComments(comments.filter((comment) => comment._id !== id));
   };
   console.log(user, "test");
   return (
     <div className="PokemonPage">
-      {state.pokemon.name ? (
+      {pokemon.name ? (
         <div>
           <div className="PokemonPage-image">
             <span>
-              <img
-                height="250"
-                src={state.pokemon.sprites.front_default}
-                alt=""
-              />
+              <img height="250" src={pokemon.sprites.front_default} alt="" />
             </span>
           </div>
 
@@ -71,11 +59,11 @@ const PokemonPage = ({ user, handleFavorite }) => {
             <div className="PokemonPage-details">
               <span>Name:</span>
               <span style={{ textTransform: "capitalize" }}>
-                {state.pokemon.name}
+                {pokemon.name}
               </span>
               <span>Type(s):</span>
               <span>
-                {state.pokemon.types.map((type, idx) => (
+                {pokemon.types.map((type, idx) => (
                   <div key={idx} style={{ textTransform: "capitalize" }}>
                     {type.type.name}
                   </div>
@@ -83,51 +71,48 @@ const PokemonPage = ({ user, handleFavorite }) => {
               </span>
               <span>Height:</span>
               <span>
-                {Math.floor(state.pokemon.height / 3.048)} ft.{" "}
+                {Math.floor(pokemon.height / 3.048)} ft.{" "}
                 {Math.ceil(
                   12 *
-                    (state.pokemon.height / 3.048 -
-                      Math.floor(state.pokemon.height / 3.048))
+                    (pokemon.height / 3.048 -
+                      Math.floor(pokemon.height / 3.048))
                 )}{" "}
                 in.
               </span>
               <span>Weight:</span>
-              <span>{Math.ceil(state.pokemon.weight / 4.536)} lbs.</span>
+              <span>{Math.ceil(pokemon.weight / 4.536)} lbs.</span>
               <span>Abilities:</span>
               <span>
-                {state.pokemon.abilities.map((ability, idx) => (
+                {pokemon.abilities.map((ability, idx) => (
                   <div key={idx} style={{ textTransform: "capitalize" }}>
                     {ability.ability.name}
                   </div>
                 ))}
               </span>
               <span>Base HP:</span>
-              <span>{state.pokemon.stats[5].base_stat}</span>
+              <span>{pokemon.stats[5].base_stat}</span>
               <span>Base Attack:</span>
-              <span>{state.pokemon.stats[4].base_stat}</span>
+              <span>{pokemon.stats[4].base_stat}</span>
               <span>Base Defense:</span>
-              <span>{state.pokemon.stats[3].base_stat}</span>
+              <span>{pokemon.stats[3].base_stat}</span>
               <span>Base Special-Attack:</span>
-              <span>{state.pokemon.stats[2].base_stat}</span>
+              <span>{pokemon.stats[2].base_stat}</span>
               <span>Base Special-Defense:</span>
-              <span>{state.pokemon.stats[1].base_stat}</span>
+              <span>{pokemon.stats[1].base_stat}</span>
               <span>Base Speed:</span>
-              <span>{state.pokemon.stats[0].base_stat}</span>
+              <span>{pokemon.stats[0].base_stat}</span>
               <Link to="/" className="btn">
                 Back to Index
               </Link>
               {user ? (
                 <span className="favs">
-                  {state.pokemon.types.map((type, idx) => (
+                  {pokemon.types.map((type, idx) => (
                     <button
                       key={idx}
                       className="fav btn"
-                      onClick={() =>
-                        handleFavorite(type.type.name, state.pokemon)
-                      }
+                      onClick={() => handleFavorite(type.type.name, pokemon)}
                     >
-                      {user.favorites[`${type.type.name}`] !==
-                      state.pokemon.name
+                      {user.favorites[`${type.type.name}`] !== pokemon.name
                         ? `Make your favorite ${type.type.name} pokemon`
                         : `Get rid of my favorite ${type.type.name} pokemon`}
                     </button>
@@ -143,10 +128,10 @@ const PokemonPage = ({ user, handleFavorite }) => {
                 <form className="commentForm" onSubmit={addComment}>
                   <h4>Add Comment</h4>
                   <input
-                    type="text"
                     name="msg"
                     onChange={handleChange}
                     placeholder="Your text here"
+                    value={formData.msg}
                   ></input>
                   <input type="submit" className="btn" />
                 </form>
@@ -155,8 +140,8 @@ const PokemonPage = ({ user, handleFavorite }) => {
               )}
 
               <h3>Comments</h3>
-              {state.comments.length ? (
-                state.comments.map((comment, idx) => (
+              {comments.length ? (
+                comments.map((comment, idx) => (
                   <div className="comment" key={idx}>
                     <span>Comment:</span>
                     <span>{comment.msg}</span>
@@ -164,12 +149,14 @@ const PokemonPage = ({ user, handleFavorite }) => {
                     <span>{comment.posterName}</span>
                     <span></span>
                     {user && user._id === comment.postedBy ? (
-                      <input
-                        type="submit"
-                        value="DELETE"
-                        className="btn"
-                        onClick={() => deleteComment(comment._id)}
-                      />
+                      <div>
+                        <input
+                          type="submit"
+                          value="DELETE"
+                          className="btn"
+                          onClick={() => deleteComment(comment._id)}
+                        />
+                      </div>
                     ) : (
                       ""
                     )}
